e27b13fd574e61f5a5f5280fce20d34c5e099c6b
raghabendra <158070647+ragbendra@users.noreply.github.com>
2024-07-04 23:10:21 +0545

    Add files via upload

---
+++ jd.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'legoBlocks' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def legoBlocks(n, m):
+    # Write your code here
+
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = legoBlocks(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
